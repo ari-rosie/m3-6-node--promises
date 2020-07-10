785d816ea3eaba32b7c1b14703870acb1fe4a3a2
ari-rosie <62956748+ari-rosie@users.noreply.github.com>
2020-07-10 13:37:31 -0400

    Exercice-1

--- __workshop/exercise-1.js
+++ __workshop/exercise-1.js
@@ -4,11 +4,26 @@ const arrayOfWords = ['cucumber', 'tomatos', 'avocado'];
 const complicatedArray = ['cucumber', 44, true];
 
 const makeAllCaps = (array) => {
-  // write some code
+  promise = new Promise((resolve, reject) => {
+    for (const word in array) {
+      if (typeof array[word] === 'string')
+        array[word] = array[word].toUpperCase();
+      else
+        reject('not all words');
+    }
+    resolve(array);
+
+  })
+  return promise;
 };
 
 const sortWords = (array) => {
-  // write some code
+  promise = new Promise((resolve, reject) => {
+    array.sort();
+    resolve(array);
+    reject("can't be sorted");
+  }) 
+  return promise;
 };
 
 // Calling (testing)
@@ -21,3 +36,4 @@ makeAllCaps(complicatedArray)
   .then(sortWords)
   .then((result) => console.log(result))
   .catch((error) => console.log(error));
+
