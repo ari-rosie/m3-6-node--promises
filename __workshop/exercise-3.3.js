36bbaf4cdd0c4c15522eb7af35595e59e51030bf
ari-rosie <62956748+ari-rosie@users.noreply.github.com>
2020-07-12 14:58:26 -0400

    Exercice-3.3

--- __workshop/exercise-3.3.js
+++ __workshop/exercise-3.3.js
@@ -6,13 +6,18 @@ require('dotenv').config();
 
 function getAddressFromPosition(lat, lng) {
   const requestObj = {
-    key: '<MY_API_KEY>',
-    q: '<QUERY_STRING>',
+    key: process.env.OPENCAGE_API_KEY,
+    q: lat + ', ' + lng,
   };
 
-  // return ...
+  return new Promise((resolve, reject) => {
+    resolve(opencage.geocode(requestObj));
+  })
+  .then(data => data.results[0])
+  .catch(err => console.log('Error: ', err))
+  
 }
 
 getAddressFromPosition('48.8584', '2.2945').then((response) =>
-  console.log(response)
+  console.log(response.formatted)
 );
