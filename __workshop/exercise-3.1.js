7d87156334e5fa7085fd3a65a619d1c2c6e88167
ari-rosie <62956748+ari-rosie@users.noreply.github.com>
2020-07-12 14:06:13 -0400

    Exercice-3.1

--- __workshop/exercise-3.1.js
+++ __workshop/exercise-3.1.js
@@ -2,8 +2,15 @@
 // -----------------------------
 
 // require the 'request-promise' module.
+const request = require('request-promise');
 
 // Returns the current position of the ISS
 const getIssPosition = () => {
-  // write some code...
+  return new Promise((resolve, reject) => {
+    resolve(request('http://api.open-notify.org/iss-now.json'));
+  })
+  .then((response) => JSON.parse(response))
+  .catch((err) => console.log('Error: ', err))
 };
+
+getIssPosition().then((result) => console.log(result));
